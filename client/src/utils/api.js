df2f9971bb88ebb3a5c17187d6cb85d861707d33
VanshajSingh12 <vanshajsingh272@gmail.com>
2026-04-20 00:47:05 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_URL || '',
+    // baseURL: import.meta.env.VITE_APP_URL || '',
+    baseURL: 'https://trackai.onrender.com' || '',
 });
 
 export default api;
